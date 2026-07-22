cf64f687eb1d3897f6400a353a1a5a93c042620e
John George <johngeo2009@gmail.com>
2026-07-22 20:43:22 +0200

    Refactor primesum method and update main method
    Refactored primesum method to include prime checking logic directly and removed isPrime method. Updated main method to default maxNum to 20.

--- PrimeSum.java
+++ PrimeSum.java
@@ -2,7 +2,7 @@ package javaApp;
 
 public class PrimeSum {
 
-    // Method expected by PrimeSumTest.java
+    // Calculates the sum of prime numbers up to maxNum
     public static int primesum(int maxNum) {
         if (maxNum <= 0) {
             return 0;
@@ -10,35 +10,30 @@ public class PrimeSum {
 
         int sum = 0;
         for (int i = 2; i <= maxNum; i++) {
-            if (isPrime(i)) {
-                sum += i;
+            boolean isPrime = true;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                sum = sum + i;
             }
         }
         return sum;
     }
 
-  
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
-        int maxNum = 0;
+        int maxNum = 20;
         if (args != null && args.length > 0) {
             try {
                 maxNum = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                maxNum = 0;
+            } catch (Exception e) {
+                maxNum = 20;
             }
         }
-        System.out.println(primesum(maxNum));
+        int sum = primesum(maxNum);
+        System.out.println(sum);
     }
 }
